bd3ccc4f7a60b02b69769fd2c32c9424b48cbcf7
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-01 13:03:23 +0600

    madlibs project is completed

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+famous_person = input("Enter a famous person name: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so exited all the time \
+because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
