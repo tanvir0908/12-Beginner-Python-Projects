ce7da4113e629d7f6288ae5591123dd98e24073e
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-01 13:20:14 +0600

    Guess The Number (Computer) project completed

---
+++ Guss The Number (Computer)/main.py
@@ -0,0 +1,19 @@
+import random
+
+
+def guess(low, high):
+    random_number = random.randint(low, high)
+    guess_number = 0
+    while guess_number != random_number:
+        guess_number = int(input(f"Enter a number between 1 and {high}: "))
+        if guess_number < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess_number > random_number:
+            print("Sorry, guess again. Too high.")
+    print(
+        f"🎉 Congratulation. You have guessed the number {random_number} correctly.")
+
+
+low_limit = int(input("Enter the lowest range: "))
+high_limit = int(input("Enter the highest range: "))
+guess(low_limit, high_limit)
