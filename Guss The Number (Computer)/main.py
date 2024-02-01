49e8a3f059c552cec34c010238fc6e34acc5fad2
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-01 13:23:17 +0600

    Update main.py

--- Guss The Number (Computer)/main.py
+++ Guss The Number (Computer)/main.py
@@ -5,7 +5,7 @@ def guess(low, high):
     random_number = random.randint(low, high)
     guess_number = 0
     while guess_number != random_number:
-        guess_number = int(input(f"Enter a number between 1 and {high}: "))
+        guess_number = int(input(f"Guess a number between {low} and {high}: "))
         if guess_number < random_number:
             print("Sorry, guess again. Too low.")
         elif guess_number > random_number:
