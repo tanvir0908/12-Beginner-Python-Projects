f17bd3f7276213ab3139a97ffa337e8907ae71f0
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-06 01:17:48 +0600

    Hangman game completed

--- 5. Hangman/hangman.py
+++ 5. Hangman/hangman.py
@@ -1,21 +1,21 @@
 import random
-# from words import word_list
+from words import wordList
 
-wordList = ["Apple", "Potato", "Orange"]
 lives = 6
 
 # choose a random word from that word list
 chosenWord = random.choice(wordList).lower()
+print("Chosen word:", chosenWord, end="\n\n")
 
 # make empty display
 display = []
 for i in range(len(chosenWord)):
     display += '_'
 
-print(chosenWord)
+
 print(display)
 
-while lives > 0:
+while lives != 0:
     # take user input
     guessedLetter = input("Guess a letter: ").lower()
 
@@ -27,5 +27,13 @@ while lives > 0:
 
     if guessedLetter not in chosenWord:
         lives -= 1
-    print("Lives: ", lives)
+    if lives == 0:
+        print("\nGame over.😣 You loose!")
+        break
+
+    if '_' not in display:
+        print("\nGame over.🎉 You win!")
+        break
+
+    print("\nRemaining lives: ", lives)
     print(display)
