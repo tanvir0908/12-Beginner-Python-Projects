9530e8766c11c0ab8aa82cd291cc8833ecc4fd33
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-06 21:12:15 +0600

    __str__ class function

--- Object Oriented Programming/students (using class).py
+++ Object Oriented Programming/students (using class).py
@@ -2,10 +2,13 @@ class Student:
     def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Natore", "Dhaka"]:
+        if house not in ["Dhaka", "London", "Toronto", "Delhi", "Tokyo"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+
+    def __str__(self):
+        return f"{self.name} is from {self.house}"
 
 
 def getStudent():
@@ -16,7 +19,8 @@ def getStudent():
 
 def main():
     student = getStudent()
-    print(f"{student.name} is from {student.house}")
+    # print(f"{student.name} is from {student.house}")
+    print(student)
 
 
 if __name__ == "__main__":
