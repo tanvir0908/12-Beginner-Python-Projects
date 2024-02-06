3b16b95278419e4bb0f60ff53d7eb0d41e8f820f
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-06 21:33:37 +0600

    learned class method

--- Object Oriented Programming/students (using class).py
+++ Object Oriented Programming/students (using class).py
@@ -1,26 +1,40 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, city, country):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Dhaka", "London", "Toronto", "Delhi", "Tokyo"]:
-            raise ValueError("Invalid house")
+        if city not in ["Dhaka", "London", "Toronto", "Delhi", "Tokyo"]:
+            raise ValueError("Invalid city")
         self.name = name
-        self.house = house
+        self.city = city
+        self.country = country
+
+    def getCountry(self):
+        match self.country:
+            case "Bangladesh":
+                return "🍕"
+            case "India":
+                return "🍔"
+            case "England":
+                return "🌭"
+            case _:
+                return "🍿"
 
     def __str__(self):
-        return f"{self.name} is from {self.house}"
+        return f"{self.name} is from {self.city} which is a city of {self.country}"
 
 
 def getStudent():
     name = input("Enter your name: ")
-    house = input("Enter your hometown: ")
-    return Student(name, house)
+    city = input("Enter your hometown: ")
+    country = input("Enter your country: ")
+    return Student(name, city, country)
 
 
 def main():
     student = getStudent()
-    # print(f"{student.name} is from {student.house}")
-    print(student)
+    # print(f"{student.name} is from {student.city}")
+    # print(student)
+    print("Country emoji:", student.getCountry())
 
 
 if __name__ == "__main__":
