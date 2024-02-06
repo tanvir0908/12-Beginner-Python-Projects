d0b217c3b9f255b44a9bed6e8994ebec93ac53b8
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-02-07 00:07:30 +0600

    Property in Python

--- Object Oriented Programming/students (using class).py
+++ Object Oriented Programming/students (using class).py
@@ -1,40 +1,42 @@
 class Student:
-    def __init__(self, name, city, country):
-        if not name:
-            raise ValueError("Missing name")
-        if city not in ["Dhaka", "London", "Toronto", "Delhi", "Tokyo"]:
-            raise ValueError("Invalid city")
+    def __init__(self, name, city):
         self.name = name
         self.city = city
-        self.country = country
-
-    def getCountry(self):
-        match self.country:
-            case "Bangladesh":
-                return "🍕"
-            case "India":
-                return "🍔"
-            case "England":
-                return "🌭"
-            case _:
-                return "🍿"
 
     def __str__(self):
-        return f"{self.name} is from {self.city} which is a city of {self.country}"
+        return f"{self.name} is from {self.city}."
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, city):
+        if city not in ["Dhaka", "London", "Toronto", "Delhi", "Tokyo"]:
+            raise ValueError("Invalid city")
+        self._city = city
 
 
 def getStudent():
-    name = input("Enter your name: ")
-    city = input("Enter your hometown: ")
-    country = input("Enter your country: ")
-    return Student(name, city, country)
+    name = input("Enter your name: ").title()
+    city = input("Enter your hometown: ").title()
+    return Student(name, city)
 
 
 def main():
     student = getStudent()
     # print(f"{student.name} is from {student.city}")
-    # print(student)
-    print("Country emoji:", student.getCountry())
+    print(student)
 
 
 if __name__ == "__main__":
